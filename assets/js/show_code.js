622f0aea2f04bdf1115fe944e206a613dd520f65
Ben <bdjohnson529@gmail.com>
2021-10-16 16:39:47 -0700

    feat: add git_player.js

--- assets/js/show_code.js
+++ assets/js/show_code.js
@@ -2,8 +2,6 @@
 ---
 
 function ToggleElement(element_id) {
-  console.log("Within toggle element");
-
   var element = document.getElementById(element_id);
 
   console.log(element.classList);
@@ -16,15 +14,11 @@ function ToggleElement(element_id) {
 }
 
 function HideElement(element_id) {
-  console.log("Within hide element");
-
   var element = document.getElementById(element_id);
   element.classList.add("d-none");
 }
 
 function ShowElement(element_id) {
-  console.log("Within show element");
-
   var element = document.getElementById(element_id);
   element.classList.remove("d-none");
 }
